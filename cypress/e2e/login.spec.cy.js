1c78f003cc99d76189d43cfc6e5e4faa09c92e29
Leonardo Mendes <mendes.leonardo@yahoo.com>
2025-04-14 19:07:31 -0300

    added user data list for passwords and usernames

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -9,18 +9,29 @@ describe('Orange HRM - Learning Tests', () => {
     wrongCredentialAlert: '.oxd-alert'
   }
 
+  const userData = {
+    validUser: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    invalidUser: {
+      username: 'testing_user',
+      password: 'testing_pass'
+    }
+  }
+
   it('VALID LOGIN', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.validUser.username)
+    cy.get(selectorsList.passwordField).type(userData.validUser.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid).contains('Dashboard')
   })
   it('INVALID LOGIN (USERNAME)', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('wrong_username')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.invalidUser.username)
+    cy.get(selectorsList.passwordField).type(userData.invalidUser.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
